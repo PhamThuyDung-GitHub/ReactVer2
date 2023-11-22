3ec241597c32e44295d3d06ddc03b98024bec00a
amenosakura <114124231+amenosakura@users.noreply.github.com>
2023-11-23 01:39:58 +0700

    new update

--- ex13/src/peopleData.js
+++ ex13/src/peopleData.js
@@ -1,9 +1,14 @@
 const peopleData = [
-    { id: 1, name: "John Doe", address: "123 Main St", department: "HR" },
-    { id: 2, name: "Jane Smith", address: "456 Oak St", department: "IT" },
-    // ... other people data
-  ];
-  
-  export default peopleData;
+  { id: 1, name: "John Doe", address: "123 Main St", department: "HR" },
+  { id: 2, name: "Jane Smith", address: "456 Oak St", department: "IT" },
+  { id: 3, name: "Bob Johnson", address: "789 Pine St", department: "Finance" },
+  { id: 4, name: "Alice Williams", address: "101 Cedar St", department: "Marketing" },
+  { id: 5, name: "Charlie Brown", address: "202 Elm St", department: "Operations" },
+  { id: 6, name: "Eva Davis", address: "303 Birch St", department: "HR" },
+  { id: 7, name: "Frank Miller", address: "404 Maple St", department: "IT" },
+  { id: 8, name: "Grace Wilson", address: "505 Walnut St", department: "Finance" },
+  { id: 9, name: "Henry Lee", address: "606 Chestnut St", department: "Marketing" },
+  { id: 10, name: "Isabel Garcia", address: "707 Pine St", department: "Operations" },
+];
 
-  
+export default peopleData;
