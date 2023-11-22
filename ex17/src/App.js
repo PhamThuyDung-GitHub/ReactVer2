e9f40a52ff22acf34565956ebe2be4124c043faf
amenosakura <114124231+amenosakura@users.noreply.github.com>
2023-11-23 01:59:50 +0700

    repair something

--- ex17/src/App.js
+++ ex17/src/App.js
@@ -1,14 +1,40 @@
 import './App.css';
-import DatingCards from './DatingCards';
-import Header from './Header';
-import SwipeButtons from './SwipeButtons';
+import { useState } from 'react';
+import useMessages from './useMessages';
 
 function App() {
+  const [forum, setForum] = useState('nasa');
+  const {
+    data: messages,
+    loading: messagesLoading,
+    error: messagesError,
+  } = useMessages(forum);
+
   return (
-    <div className="app">
-      <Header />
-      <DatingCards />
-      <SwipeButtons />
+    <div className="App">
+      <button onClick={() => setForum('nasa')}>NASA</button>
+      <button onClick={() => setForum('notNasa')}>Not NASA</button>
+      {messagesError ? (
+        <div className="error">
+          <div className="error-contents">
+            Something went wrong:
+            <div className="error-message">{messagesError.message}</div>
+          </div>
+        </div>
+      ) : messagesLoading ? (
+        <div className="loading">Loading...</div>
+      ) : messages && messages.length ? (
+        <dl>
+          {messages.map((m) => (
+            <React.Fragment key={m.id}>
+              <dt>{m.author}</dt>
+              <dd>{m.text}</dd>
+            </React.Fragment>
+          ))}
+        </dl>
+      ) : (
+        'No messages'
+      )}
     </div>
   );
 }
